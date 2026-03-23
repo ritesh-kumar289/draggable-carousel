9805759257cd887489946520edb35f69912e6089
Ritesh Kumar <07418002722@delhitechnicalcampus.ac.in>
2026-03-23 18:00:49 +0530

    Push src/App.jsx file with new application structure on 2026-03-23 12:24:05 UTC.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import DraggableCarousel from './components/DraggableCarousel'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <DraggableCarousel />
+    </div>
+  )
+}
+
+export default App
